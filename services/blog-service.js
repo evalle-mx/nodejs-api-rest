16473657295a79a455573a2c3d1aa50896e9681f
Ernesto Valle <evalle.mx@gmail.com>
2022-03-25 13:43:31 -0400

    Segmentando codigo Controller en Service y Model

---
+++ services/blog-service.js
@@ -0,0 +1,23 @@
+const { blogs } = require('../models/blog-model')
+
+//Setting a Standard on the response
+const genericResponse  = {data:{}};
+
+//Receives queryParams
+//exports.findAll = (req, res ) => {
+exports.findAll = (username ) => {
+    genericResponse.data = blogs;
+    if(username){
+        genericResponse.data = blogs.filter( blog => blog.username === username); 
+    }
+    //return res.status(200).json(genericResponse).end();
+    return genericResponse;
+}
+//Function that receives params as URL path
+// exports.findById = (req, res ) => {
+exports.findById = ( id ) => {    
+    const blog = blogs.find( blog => blog.id === Number(id) ); //Convert in same type
+    genericResponse.data = blog;
+    //return res.status(200).json(genericResponse).end();
+    return genericResponse;
+} 
